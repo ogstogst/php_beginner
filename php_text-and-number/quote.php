18386a350a38e4ed655dfbcda4024542b4780de6
ogstogst <ogst2266@gmail.com>
2024-02-10 00:33:31 +0900

    24/2/10

---
+++ php_text-and-number/quote.php
@@ -0,0 +1,14 @@
+<?php
+echo 'he\nll\to';
+echo "<br>";
+echo "he\nll\to";
+echo "<br>";
+echo "\n ←これ改行しないけど。";
+echo "<br>";
+echo '\nのことね。';
+echo "<br>";
+
+$test = "ねみー";
+echo "あー$test";
+echo "<br>";
+echo 'あー$test';
